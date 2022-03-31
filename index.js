a6baf2123db1dac15ddb360cad78f20daadc637b
Alef Barbosa <contatoalef@hotmail.com>
2022-03-31 20:56:50 -0300

    commit_radio1

---
+++ index.js
@@ -0,0 +1,6 @@
+const cover = document.querySelector(".card-image");
+const title = document.querySelector(".card-content h5");
+const artist = document.querySelector(".artist");
+const audio = document.querySelector("audio");
+
+alert('Em caso de falha no player, comunicar através das redes sociais pressionando o botão "REDES SOCIAIS", aproveite e siga-nos!.')
